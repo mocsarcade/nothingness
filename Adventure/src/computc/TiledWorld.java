0e3ab0f6fbec46ee78e0f01a514b7b527b98dc05
Andrew McPherson <andrewmcp333@gmail.com>
2014-08-25 19:49:54 -0400

    Iterated through the map for tile properties.

--- Adventure/src/computc/TiledWorld.java
+++ Adventure/src/computc/TiledWorld.java
@@ -5,18 +5,33 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class TiledWorld extends TiledMap
 {
+	private Tile[][] tiles;
+	
 	public TiledWorld(String tmx) throws SlickException
 	{
 		super(tmx);
+		
+		tiles = new Tile[this.getWidth()][this.getHeight()];
+
+		for(int x = 0; x < this.getWidth(); x++)
+		{
+			for(int y = 0; y < this.getHeight(); y++)
+			{
+				int tid = this.getTileId(x, y, 0);
+				
+				tiles[x][y] = new Tile();
+				tiles[x][y].collider = this.getTileProperty(tid, "collider", "false").equals("false");
+			}
+		}
 	}
 	
 	public int getPixelWidth()
 	{
-		return getWidth() * getTileWidth();
+		return this.getWidth() * this.getTileWidth();
 	}
 	
 	public int getPixelHeight()
 	{
-		return getHeight() * getTileHeight();
+		return this.getHeight() * this.getTileHeight();
 	}
 }
