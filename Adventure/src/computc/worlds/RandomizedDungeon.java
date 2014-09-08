9834fd6d789ad1607a33f900a7ade5218408f3c3
Andrew McPherson <andrewmcp333@gmail.com>
2014-09-07 21:07:11 -0400

    Implemented a method to randomly get a direction.

--- Adventure/src/computc/worlds/RandomizedDungeon.java
+++ Adventure/src/computc/worlds/RandomizedDungeon.java
@@ -1,11 +1,17 @@
 package computc.worlds;
 
+import java.util.Random;
+
 import org.newdawn.slick.SlickException;
+
+import computc.Direction;
 
 public class RandomizedDungeon extends Dungeon
 {
 	public RandomizedDungeon() throws SlickException
 	{
-		this.addRoom(new Room(this, 0, 0, "empty"));
+		Room room = new Room(this, 0, 0, "empty");
+		
+		this.addRoom(room);
 	}
 }
