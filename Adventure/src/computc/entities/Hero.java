d492088efc9c650ffdb87858e0108bee0a77deba
Forrest Pruitt <forrest-pruitt@mocs.utc.edu>
2014-10-21 13:43:56 -0400

    Footstep sounds now work.

--- Adventure/src/computc/entities/Hero.java
+++ Adventure/src/computc/entities/Hero.java
@@ -197,19 +197,22 @@ public class Hero extends Entity
 		if(sprite == walkingUp)
 		{
 			walkingUp.draw(this.getX() - this.getHalfWidth() - camera.getX(), this.getY() - this.getHalfHeight() - camera.getY());
-			Game.assets.playSoundEffectWithoutRepeat("footstep");
+			playFootstepSound();
 		}
 		else if(sprite == walkingDown)
 		{
 			walkingDown.draw(this.getX() - this.getHalfWidth() - camera.getX() , this.getY() - this.getHalfHeight() - camera.getY());
+			playFootstepSound();
 		}
 		else if(sprite == walkingRight)
 		{
 			walkingRight.draw(this.getX() - this.getHalfWidth() - camera.getX() , this.getY() - this.getHalfHeight() - camera.getY());
+			playFootstepSound();
 		}
 		else if(sprite == walkingLeft)
 		{
 			walkingLeft.draw(this.getX() - this.getHalfWidth() - camera.getX() , this.getY() - this.getHalfHeight() - camera.getY());
+			playFootstepSound();
 		}
 		else if(sprite == idle && (this.direction == Direction.SOUTH || this.direction == Direction.NONE))
 		{
@@ -247,6 +250,18 @@ public class Hero extends Entity
 		
 		// converts box2d position to hero's position on screen
 		box2dPlayerPosition = new Vec2(this.getLocalX(camera)/30, this.getLocalY(camera)/30);
+	}
+	
+	private int framesSinceLastFootstep = 0;
+	private void playFootstepSound()
+	{
+		framesSinceLastFootstep++;
+		if(framesSinceLastFootstep > 20)
+		{
+			Game.assets.playSoundEffectWithoutRepeat("footstep");
+			framesSinceLastFootstep= 0;
+		}
+		
 	}
 	
 	public void renderOnMap(Graphics graphics, Camera camera)
